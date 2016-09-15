f705cde184875e8b1af316801264365f5bac0756
Brent Haas <brenthaas@gmail.com>
2016-09-14 22:52:48 -0700

    Upgraded Rails to 5.0.0.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :wishes, foreign_key: :wisher_id, inverse_of: :wisher, dependent: :destroy
 end
