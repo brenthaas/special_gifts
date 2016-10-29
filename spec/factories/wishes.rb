354e9101805a4698e56120b5a18996050dd3753b
Brent Haas <brenthaas@gmail.com>
2016-10-28 17:49:23 -0700

    Added annotations

--- spec/factories/wishes.rb
+++ spec/factories/wishes.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: wishes
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  description :text
+#  url         :text
+#  wisher_id   :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 FactoryGirl.define do
   factory :wish do
     association :wisher, factory: :user
--- spec/models/wish_spec.rb
+++ spec/models/wish_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: wishes
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  description :text
+#  url         :text
+#  wisher_id   :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe Wish do
