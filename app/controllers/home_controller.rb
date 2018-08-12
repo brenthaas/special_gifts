903d73472fbce03be94359001ded003d36eec2d9
Brent <brenthaas@gmail.com>
2018-08-12 01:24:24 -0700

    check for authentication

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,9 @@
 class HomeController < ApplicationController
+  before_action :check_auth
+
+  private
+
+  def check_auth
+    redirect_to new_user_session_url unless user_signed_in?
+  end
 end
---
+++ spec/requests/homes_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "Homes", type: :request do
+  describe "GET /" do
+    subject { get '/' }
+
+    context 'when the user is NOT logged in' do
+      it "redirects to the sign_in page" do
+        is_expected.to redirect_to new_user_session_url
+      end
+    end
+
+    context 'when the user is signed in' do
+      let(:user) { FactoryBot.create(:user) }
+
+      before do
+        sign_in user
+        get '/'
+      end
+
+      it { is_expected.to have_http_status :ok }
+    end
+  end
+end
