ffcf6252074b09aa11375a83283a4202432e3927
Brent <brenthaas@gmail.com>
2019-09-18 15:44:21 -0700

    add Home controller for base index

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,11 @@
+class HomeController < ApplicationController
+  before_action :check_auth
+
+  def index; end
+
+  private
+
+  def check_auth
+    redirect_to new_user_session_url unless user_signed_in?
+  end
+end
