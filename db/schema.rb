6f8a7380976813621930342d9693296ae686ca99
Brent <brenthaas@gmail.com>
2019-09-18 16:42:57 -0700

    auto-correct cops

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2019_09_18_213817) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,5 +41,4 @@ ActiveRecord::Schema.define(version: 2019_09_18_213817) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["wisher_id"], name: "index_wishes_on_wisher_id"
   end
-
 end
--- spec/factories/wishes.rb
+++ spec/factories/wishes.rb
@@ -14,7 +14,7 @@
 FactoryBot.define do
   factory :wish do
     association :wisher, factory: :user
-    
+
     title       { Faker::Hipster.sentence }
     description { Faker::Hipster.paragraph }
     url         { Faker::Internet.url }
