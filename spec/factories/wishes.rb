0ef512e26a138db65d1b20e4b7ada183490e70d2
Brent <brenthaas@gmail.com>
2019-09-18 15:43:30 -0700

    add specs from special_gifts

---
+++ spec/factories/wishes.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: wishes
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  description :text
+#  url         :text
+#  wisher_id   :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+FactoryBot.define do
+  factory :wish do
+    association :wisher, factory: :user
+    
+    title       { Faker::Hipster.sentence }
+    description { Faker::Hipster.paragraph }
+    url         { Faker::Internet.url }
+  end
+end
---
+++ spec/models/wish_spec.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: wishes
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  description :text
+#  url         :text
+#  wisher_id   :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+require 'rails_helper'
+
+RSpec.describe Wish do
+  it { is_expected.to validate_presence_of(:wisher) }
+  it { is_expected.to validate_presence_of(:title) }
+end
