ed7903e96e10f1128b3498eea7a477c3899a0748
Brent <brenthaas@gmail.com>
2019-10-14 14:12:32 -0700

    make generalized not_found method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  protected
+
+  def not_found
+    render plain: "404 Not Found", status: 404
+  end
 end
