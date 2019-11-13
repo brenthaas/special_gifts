888c04070dd7e699c7bef3a63caa15228dbd3c90
Brent <brenthaas@gmail.com>
2019-11-13 12:29:24 -0800

    use hooks - it's the future! ;)

--- app/javascript/components/WishesApp.js
+++ app/javascript/components/WishesApp.js
@@ -1,50 +1,39 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import WishForm from './WishForm'
 
-class WishesApp extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      myWishes: [],
-      isLoading: true
+function WishesApp() {
+  const [myWishes, setMyWishes] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      fetch('http://localhost:3000/api/v1/users/1/wishes')
+        .then(response => response.json())
+        .then((data) => {
+          setMyWishes(data)
+        });
     };
-  }
+    fetchData();
+  });
 
-  render() {
-    if (this.state.isLoading) {
-      return ( <p> Loading... </p> )
-    }
-    
-    return (
-      <div className="container">
-        <div className="header">
-          <h1>Wish List</h1>
-          <div>
-            <ul>
-              { this.state.myWishes.map( wish =>
-                <li key={wish.title} className='wish-item'>
-                  <span className='wish-title'>{wish.title}</span>
-                  &nbsp; &mdash; &nbsp;
-                  <span className='wish-description'>{wish.description}</span>
-                </li>
-              )}
-            </ul>
-          </div>
+  return (
+    <div className="container">
+      <div className="header">
+        <h1>Wish List</h1>
+        <div>
+          <ul>
+            { myWishes.map( wish =>
+              <li key={wish.title} className='wish-item'>
+                <span className='wish-title'>{wish.title}</span>
+                &nbsp; &mdash; &nbsp;
+                <span className='wish-description'>{wish.description}</span>
+              </li>
+            )}
+          </ul>
         </div>
-        <WishForm />
       </div>
-    );
-  }
-
-  componentDidMount() {
-    this.state.isLoading = true
-
-    fetch('http://localhost:3000/api/v1/users/1/wishes')
-      .then(response => response.json())
-      .then((data) => {
-        this.setState({ myWishes: data, isLoading: false })
-      })
-  }
+      <WishForm />
+    </div>
+  );
 }
 
 export default WishesApp;
