cb05f7135b9d72dc210492dc50e1965cc57d44df
Brent <brenthaas@gmail.com>
2019-11-15 12:23:01 -0800

    add reac-router

--- app/javascript/components/WishesApp.js
+++ app/javascript/components/WishesApp.js
@@ -1,20 +1,78 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import WishForm from './WishForm'
 import MyWishList from './MyWishList'
 import { MyWishesContextProvider } from './../contexts/MyWishesContext';
 
-function WishesApp() {
-  return (
-    <MyWishesContextProvider>
-      <div className='container'>
+const routes = [
+  {
+    path: "/",
+    exact: true,
+    sidebar: () => <div>home!</div>,
+    main: () => <h2>Home</h2>
+  },
+  {
+    path: "/my_wishes",
+    sidebar: () => <div>My Wishes</div>,
+    main: () => {
+      return (
         <div className='header'>
           <h1>Wish List</h1>
           <div>
             <MyWishList />
           </div>
         </div>
+      );
+    }
+  },
+  {
+    path: "/add_wish",
+    sidebar: () => <div>Add a Wish</div>,
+    main: () => {
+      return (
         <WishForm />
-      </div>
+      );
+    }
+  }
+];
+
+function WishesApp() {
+  return (
+    <MyWishesContextProvider>
+      <Router>
+        <div className='sidebar'>
+          <ul className='sidebar-nav'>
+            <li className='sidebar-nav-item'>
+              <Link to='/'>Home</Link>
+            </li>
+            <li className='sidebar-nav-item'>
+              <Link to='/my_wishes'>My Wishes</Link>
+            </li>
+            <li className='sidebar-nav-item'>
+              <Link to='/add_wish'>Add A Wish</Link>
+            </li>
+          </ul>
+        </div>
+
+        <div className='main'>
+          <Switch>
+            {routes.map((route, index) => (
+              <Route
+                key={index}
+                path={route.path}
+                exact={route.exact}
+                children={<route.main />}
+              />
+            ))}
+          </Switch>
+        </div>
+      </Router>
     </MyWishesContextProvider>
   );
 }
