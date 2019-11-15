35b2445f8d48a669f1a8f354727ba3aa8afed7f7
Brent <brenthaas@gmail.com>
2019-11-15 11:28:18 -0800

    changed double quotes to singles to be consistent

--- app/javascript/components/WishForm.js
+++ app/javascript/components/WishForm.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react"
-import PropTypes from "prop-types"
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class WishForm extends Component {
   render () {
